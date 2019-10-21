6dc8e60974eccf1ae7ad500763b3cd58aeaf446b
Stanislav Lebedkin <slebedkin90@gmail.com>
2019-10-21 23:48:53 +0300

    first commit

---
+++ src/main/java/ru/yamal/barbos/BarbosApplication.java
@@ -0,0 +1,13 @@
+package ru.yamal.barbos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbosApplication.class, args);
+    }
+
+}
